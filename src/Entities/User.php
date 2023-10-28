4f0d65f4fad509be28491d72da8603fa2c6cc789
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-28 14:28:40 -0300

    Aula 07 - Métodos acessores dos atributos da entidade

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -21,4 +21,20 @@ class User
      */
     private string $name;
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+
 }
