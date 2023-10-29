04d9efe27f2eed8a906cffee6c1d73d4ace9b2d9
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-29 15:38:40 -0300

    Aula 14 - Query Builder

--- search-user.php
+++ search-user.php
@@ -6,12 +6,27 @@ use App\Helper\EntityManagerCreator;
 require __DIR__ . "/vendor/autoload.php";
 
 $em = EntityManagerCreator::createEntityManager();
+$userEntity = User::class;
 
-$query = $em->createQuery("SELECT u FROM " . User::class. " u");
+$query = $em->createQuery("SELECT u FROM {$userEntity} u");
 $allUsers = $query->getResult();
+
+//var_dump($allUsers);
+
+$query = $em->createQuery("SELECT u FROM " . User::class . " u WHERE u.id > :number");
+$query->setParameter("number", $_GET['num'] ?? 1);
+$allUsers = $query->getResult();
+
+//var_dump($allUsers);
+
+$qb = $em->createQueryBuilder();
+$qb->select('u')
+    ->from($userEntity, "u")
+    ->where('u.id > :number')
+    ->setParameter('number', $_GET['num'] ?? 0);
+
+$queryDql = $qb->getQuery();
+$allUsers = $queryDql->getResult();
 
 var_dump($allUsers);
 
-$query = $em->createQuery("SELECT u FROM " . User::class. " u WHERE u.id > :number");
-$query->setParameter("number", $_GET['num']);
-$allUsers = $query->getResult();
